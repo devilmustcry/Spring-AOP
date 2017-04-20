605cc589b717771a66d652a43e979b9c47a4040d
devilmustcry <eaglization@gmail.com>
2017-04-20 16:36:51 +0700

    Send Assignment

---
+++ src/sample/spring/BankAccountService.java
@@ -0,0 +1,8 @@
+package sample.spring;
+
+/**
+ * Created Arut Thanomwatana on 4/10/2017 AD.
+ */
+public interface BankAccountService {
+    int createBankAccount(BankAccountDetails bankAccountDetails);
+}
